2c9d88228b69ed93024b76c8addb839decc7674a
Tay Jun Yang <danny_tayjunyang@hotmail.com>
2022-09-28 06:31:31 +0800

    Implement snackbar alert for delete account
    - Not fully working due to navigating to baseUrl

--- frontend/src/components/DeleteAccountDialog.js
+++ frontend/src/components/DeleteAccountDialog.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {
   Button,
@@ -8,36 +8,61 @@ import {
   DialogContentText,
   DialogTitle,
 } from '@mui/material'
-import { UserContext } from '../contexts/UserContext'
 import { deleteUser } from '../api/userService'
+import { UserContext } from '../contexts/UserContext'
 import { baseUrl } from '../utils/routeConstants'
+import SnackbarAlert from './SnackbarAlert'
 
 const DeleteAccountDialog = ({ dialogOpen, handleNo }) => {
-  const navigate = useNavigate()
   const user = useContext(UserContext)
+  const navigate = useNavigate()
+
+  // Delete Account Successful Alert
+  const [
+    deleteAccountSuccessfulAlertOpen,
+    setDeleteAccountSuccessfulAlertOpen,
+  ] = useState(false)
+  const handleDeleteAccountSuccessfulOpenAlert = () =>
+    setDeleteAccountSuccessfulAlertOpen(true)
 
   const handleDeleteAccount = async () => {
     try {
       await deleteUser(user.username)
       navigate(baseUrl)
+
+      // The alert will not show as the parent component (ProfilePage)
+      // is killed after navigating to baseUrl. Need to think and discuss
+      // a workaround for this.
+      handleDeleteAccountSuccessfulOpenAlert()
     } catch (err) {
       console.log(`Failed to delete user account: ${err}`)
     }
   }
 
   return (
-    <Dialog open={dialogOpen}>
-      <DialogTitle>Delete Account</DialogTitle>
-      <DialogContent dividers>
-        <DialogContentText>
-          Are you sure you want to delete your account?
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleNo}>No</Button>
-        <Button onClick={handleDeleteAccount}>Yes</Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <div>
+        <Dialog open={dialogOpen}>
+          <DialogTitle>Delete Account</DialogTitle>
+          <DialogContent dividers>
+            <DialogContentText>
+              Are you sure you want to delete your account?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleNo}>No</Button>
+            <Button onClick={handleDeleteAccount}>Yes</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+
+      <SnackbarAlert
+        alertOpen={deleteAccountSuccessfulAlertOpen}
+        setAlertOpen={setDeleteAccountSuccessfulAlertOpen}
+        severity="success"
+        alertMsg="Your account has been deleted successfully!"
+      />
+    </>
   )
 }
 
